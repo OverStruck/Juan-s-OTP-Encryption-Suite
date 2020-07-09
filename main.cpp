881b8a9b00d0c24f1e17117328109ba6ea90696e
OverStruck <OverStruck@users.noreply.github.com>
2020-07-08 17:35:07 -0700

    Add sanity additional sanity checks
    Add aditional sanity check for the seed number. Previously seed was hardcoded to be 4, no user can input their own seed and two extra sanity checks have been added. Seed must be greater than 1 & less than N-1. And P & Q must be of equal length.

--- main.cpp
+++ main.cpp
@@ -39,30 +39,51 @@ bool checkParams(const string p, const string q, const string seed)
 	printf("CHECKING P & Q...");
 	printf(CYAN "\n======================================================\n" RESET);
 
-    mpz_t gmpP, gmpQ, gmpSeed, gmpN, gmpPQ_GCD, gmpSN_GCD;
+	//GMP variables to hold large numbers
+    mpz_t gmpP, gmpQ, gmpSeed, gmpN, gmpNminus1, gmpPQ_GCD, gmpSN_GCD;
 
-    mpz_inits(gmpP, gmpQ, gmpSeed, gmpN, gmpPQ_GCD, gmpSN_GCD, NULL);
+    mpz_inits(gmpP, gmpQ, gmpSeed, gmpN, gmpNminus1, gmpPQ_GCD, gmpSN_GCD, NULL);
+
+	//initialize values using GMP in base 10
     mpz_set_str(gmpP, p.c_str(), 10);
     mpz_set_str(gmpQ, q.c_str(), 10);
     mpz_set_str(gmpSeed, seed.c_str(), 10);
+	//multiply P * Q to get N
     mpz_mul(gmpN, gmpP, gmpQ);
+	//set gmpNminus1 to N - 1 for seed sanity check
+	mpz_sub_ui(gmpNminus1, gmpN, 1);
+	//compute GCD of P and Q
     mpz_gcd(gmpPQ_GCD, gmpP, gmpQ);
+	//compute GCD of Seed and N
     mpz_gcd(gmpSN_GCD, gmpSeed, gmpN);
-
+	//check P & Q are prime numbers using GPM
 	string isPprime = isPrime(gmpP);
 	string isQprime = isPrime(gmpQ);
+
+	/*
+		mpz_fdiv_ui returns an unsigned long int
+		compute the reminder (mod) of P mod 4
+	*/
 	unsigned long int pMod4 = mpz_fdiv_ui(gmpP, 4);
 	unsigned long int qMod4 = mpz_fdiv_ui(gmpQ, 4);
 
-    gmp_printf("P is: %d bits long\n", mpz_sizeinbase(gmpP, 2));
-	gmp_printf("Q is: %d bits long\n", mpz_sizeinbase(gmpQ, 2));
+	//size in bits for P & Q required for sanity check
+	// |P| = |Q| is required
+	size_t pSize = mpz_sizeinbase(gmpP, 2);
+	size_t qSize = mpz_sizeinbase(gmpQ, 2);
+	
+	//print the results
+	gmp_printf("P is: %d bits long\n", pSize);
+	gmp_printf("Q is: %d bits long\n", qSize);
 	gmp_printf("N is: %d bits long\n", mpz_sizeinbase(gmpN, 2));
 	gmp_printf("The modulus N = P * Q is: %Zd\n", gmpN);
 
     cout << "is P prime? " << isPprime  << endl;
     cout << "is Q prime? " << isQprime  << endl;
+
     gmp_printf("Greatest Common Demoninator of P & Q is: %Zd\n", gmpPQ_GCD);
     gmp_printf("Greatest Common Demoninator of S & N is: %Zd\n", gmpSN_GCD);
+
     printf("P mod 4 is: %lu\n",  pMod4);
     printf("Q mod 4 is: %lu\n",  qMod4);
 
@@ -71,37 +92,56 @@ bool checkParams(const string p, const string q, const string seed)
 
 	printf(RED "======================================================\n");
 
+	//return value of this function
 	bool result = true;
 
 	if (isPprime == "No" || isQprime == "No")
 	{
-		printf("P or Q are NOT prime numbers...\n");
+		printf("* P or Q are NOT prime numbers...\n");
 		result = false;
 	}
+
 	if (mpz_cmp_d(gmpPQ_GCD, 1) != 0)
 	{
-		printf("The GCD of P & Q is NOT 1\n");
+		printf("* The GCD of P & Q is NOT 1\n");
 		result = false;
 	}
+
 	if (mpz_cmp_d(gmpSN_GCD, 1) != 0)
 	{
-		printf("The GCD of S & N is NOT 1\n");
+		printf("* The GCD of S & N is NOT 1\n");
 		result = false;
 	}
+
 	if (pMod4 != 3)
 	{
-		printf("P is NOT congruent to 3 mod 4\n");
+		printf("* P is NOT congruent to 3 mod 4\n");
 		result = false;
 	}
+
 	if (qMod4 != 3)
 	{
-		printf("Q is NOT congruent to 3 mod 4\n");
+		printf("* Q is NOT congruent to 3 mod 4\n");
 		result = false;
 	}
+
+	if (pSize != qSize)
+	{
+		printf("* The length of P & Q is not the same! |P| = |Q| is required\n");
+		result = false;
+	}
+
+	//make sure seed > 1 && seed < N-1
+	if (mpz_cmp_si(gmpSeed, 2) < 0 || mpz_cmp(gmpSeed, gmpNminus1) > 0)
+	{
+		printf("* The Seed number is not greater than 1 or less than N-1\n");
+		result = false;
+	}
+
 	printf("======================================================\n" RESET);
 
-	//clear
-    mpz_clears(gmpP, gmpQ, gmpSeed, gmpN, gmpPQ_GCD, gmpSN_GCD, NULL);
+	//clear GMP variables
+    mpz_clears(gmpP, gmpQ, gmpSeed, gmpN, gmpNminus1, gmpPQ_GCD, gmpSN_GCD, NULL);
 
 	if (result)
 	{
@@ -137,15 +177,18 @@ void decryptFile(const string p, const string q, const string seed)
 void encryptionMenu()
 {
     int action;
-	string p, q;
+	string p, q, seed;
 	cout << YELLOW << "======================================================\n"
 	<< "Juan's OTP Encryption Suite Version 0.1\n"
 	<< "======================================================" << RESET
 	<< "\nEnter the Blum Prime \"p\" you want to use for encryption/decryption: ";
 	cin >> p;
-	cout << "Enter the Blum Prime \"q\" you want to use for encryption/decryption: ";
+	cout << "\nEnter the Blum Prime \"q\" you want to use for encryption/decryption: ";
 	cin >> q;
-	if (checkParams(p, q, "4"))
+	cout << "\nEnter the Seed number you want to use for encryption/decryption: ";
+	cin >> seed;
+
+	if (checkParams(p, q, seed))
 	{
 		cout << "\nChoose task:\n"
 		<< "[1] Encrypt File\n[2] Decrypt File" << endl
@@ -154,10 +197,10 @@ void encryptionMenu()
 		switch(action)
 		{
 			case 1:
-				encryptFile(p, q, "4");
+				encryptFile(p, q, seed);
 			break;
 			case 2:
-				decryptFile(p, q, "4");
+				decryptFile(p, q, seed);
 			break;
 			default:
 				cerr << "Invalid option. Try again.";
